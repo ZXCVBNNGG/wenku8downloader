31aa59d66d64bf70ffd878eb0c0d1fad15d6c8a6
Your Name <you@example.com>
2021-03-27 11:41:23 +0800

    GUI支持

--- gui_main.py
+++ gui_main.py
@@ -1,64 +1,55 @@
 import os
 import pickle
 import sys
-from sys import path
+from os import path
 
 from PyQt5 import QtWidgets
 from requests.cookies import RequestsCookieJar
 
 from src.exceptions import LoginFailedError
-from src.gui import Ui_mainWindow
+from src.ui.Login import Ui_Dialog
+from src.ui.MainWindow import Ui_mainWindow
 from src.user import SelfUser
-from PyQt5.QtWidgets import QInputDialog
 
 
-class MainWindow(Ui_mainWindow):
+class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setupUi(self)
+
+    def logger(self, text):
+        self.Logs.append(text)
+        cursor = self.Logs.textCursor()
+        self.Logs.moveCursor(cursor.End)
+        QtWidgets.QApplication.processEvents()
+
+
+class LoginWindow(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.initUI()
+        super(LoginWindow, self).__init__()
+        self.setupUi(self)
 
-    def initUI(self):
-        self.login()
-
-    def login(self):
+    def setupUi(self, Dialog):
         cookiePath = path.dirname('.') + 'cookies.dat'
         try:
             if os.path.getsize(cookiePath) > 0:
                 cookies: RequestsCookieJar = pickle.load(open(cookiePath, 'rb'))
                 self_user = SelfUser.fromCookies(cookies)
-                self.printf("从Cookies中登录成功！")
         except FileNotFoundError:
-
-            account =
-            password = input("密码：")
-            self.printf("登录中...")
+            account = self.username.text()
+            password = self.password.text()
             try:
                 self_user = SelfUser(account, password)
-                self.printf("登录成功！")
                 cookies = self_user.cookies
-                self.printf("保存Cookies中...")
                 pickle.dump(cookies, open(cookiePath, 'wb'))
-                self.printf("保存成功！")
             except LoginFailedError:
-                self.printf("登录错误！")
-                sys.exit()
-        except LoginFailedError:
-            account = input("用户名：")
-            password = input("密码：")
-            self.printf("登录中...")
-            try:
-                self_user = SelfUser(account, password)
-                self.printf("登录成功！")
-                cookies = self_user.cookies
-                self.printf("保存Cookies中...")
-                pickle.dump(cookies, open(cookiePath, 'wb'))
-                self.printf("保存成功！")
-            except LoginFailedError:
-                self.printf("登录错误！")
-                sys.exit()
+                self.login()
 
-    def printf(self, text):
-        self.Log.append(text)
-        cursor = self.Log.textCursor()
-        self.Logs.moveCursor(cursor.End)
-        QtWidgets.QApplication.processEvents()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    l = LoginWindow()
+    l.show()
+    sys.exit(app.exec_())
